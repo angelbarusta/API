ebfc7d6251129dac0c694c906b06f65d57101916
Angel Barusta <angel.barusta.95@gmail.com>
2020-05-15 20:42:21 -0500

    OK

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "../pages/style/Home.css";
 import CatalogoPersonajes from "../components/CatalogoPersonajes";
 
 const Home = () => {
+  const [pag, setChangePage] = useState(1);
   return (
     <div className='Container__Home'>
       <section className='Menu__Bar'>
-        <button onClick={() => handleNextPage()}>Next</button>
-        <button>Prev</button>
+        <div>
+          <button onClick={() => setChangePage(pag + 1)}>Next</button>
+          {pag > 1 && (
+            <button onClick={() => setChangePage(pag - 1)}>Prev</button>
+          )}
+        </div>
       </section>
       <section className='Data__content'>
-        <CatalogoPersonajes pag={3} />
+        {pag > 0 && <CatalogoPersonajes pag={pag} />}
       </section>
     </div>
   );
