85e17679f598fcc6d7f41d1d0d8cc9be8eb1c16d
Angel Barusta <angel.barusta.95@gmail.com>
2020-05-15 19:37:50 -0500

    ok

--- webpack.dev2.config.js
+++ webpack.dev2.config.js
@@ -19,10 +19,17 @@ module.exports = {
     port: 1111,
     historyApiFallback: true,
   },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public/index.html"),
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.js$/,
+        exclude: /node_modules/,
         use: {
           loader: "babel-loader",
           options: {
@@ -33,40 +40,11 @@ module.exports = {
             ],
           },
         },
-        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
-      {
-        test: /\.less$/,
-        use: ["style-loader", "css-loader", "less-loader"],
-      },
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.styl$/,
-        use: ["style-loader", "css-loader", "stylus-loader"],
-      },
-      {
-        test: /\.jpg|png|gif|woff|eot|ttf|svg|mp4|webm$/,
-        use: {
-          loader: "file-loader", //url-loader
-          options: {
-            //limit: 90000,
-            outputPath: "assets/",
-          },
-        },
-      },
     ],
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public/index.html"),
-    }),
-  ],
 };
